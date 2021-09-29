496e58c7eb357c2f92f743c68fb54035413257c9
PiratJack <90952042+PiratJack@users.noreply.github.com>
2021-09-29 20:15:29 +0200

    Fixed 49514 - Show game options on lobby list

--- gameoptions.inc.php
+++ gameoptions.inc.php
@@ -13,18 +13,18 @@ $game_options = [
     100 => [
         'name' => totranslate('Points to win'),
         'values' => [
-            1 => ['name' => totranslate('11 points')],
-            2 => ['name' => totranslate('16 points')],
-            3 => ['name' => totranslate('21 points')],
-            4 => ['name' => totranslate('31 points')],
+            1 => ['name' => totranslate('11 points'), 'tmdisplay' => '11 points to win'],
+            2 => ['name' => totranslate('16 points'), 'tmdisplay' => '16 points to win'],
+            3 => ['name' => totranslate('21 points'), 'tmdisplay' => '21 points to win'],
+            4 => ['name' => totranslate('31 points'), 'tmdisplay' => '31 points to win'],
         ],
         'default' => 1,
     ],
     101 => [
         'name' => totranslate('Max number of cards captured'),
         'values' => [
-            1 => ['name' => totranslate('2 cards')],
-            2 => ['name' => totranslate('No limit')],
+            1 => ['name' => totranslate('2 cards'), 'tmdisplay' => 'Max of 2 cards captured'],
+            2 => ['name' => totranslate('No limit'), 'tmdisplay' => 'No limit on capture'],
         ],
         'default' => 2,
     ],
